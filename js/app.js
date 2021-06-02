442d331040a75c43310ec78d6a04f7b85a2e792b
JasonKarau <Karauski@gmail.com>
2021-06-02 14:04:01 -0500

    desktop 1440 done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+const tabs = document.querySelectorAll('.io');
+const hiddenText = document.querySelectorAll('.hidden-subtext');
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", (event) => {
+    if (hiddenText[index].style.display == "none") {
+      tab.innerHTML = "&#8593;";
+      hiddenText[index].style.display = "block";
+    } else {
+      hiddenText[index].style.display = "none";
+      tab.innerHTML = "&#8595;";
+  }});
+});
+
 document.addEventListener( 'DOMContentLoaded', function () {
     new Splide( '.splide' ).mount();
 } );
